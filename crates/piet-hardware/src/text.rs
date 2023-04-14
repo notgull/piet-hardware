6efb7d5027fb57a1823710f324351414e7d0799f
jtnunley <jtnunley01@gmail.com>
2023-04-14 16:44:30 -0700

    Add patron license

--- crates/piet-hardware/src/text.rs
+++ crates/piet-hardware/src/text.rs
@@ -1,20 +1,23 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later OR MPL-2.0
 // This file is a part of `piet-hardware`.
 //
-// `piet-hardware` is free software: you can redistribute it and/or modify it under the terms of
-// either:
+// `piet-hardware` is free software: you can redistribute it and/or modify it under the
+// terms of either:
 //
 // * GNU Lesser General Public License as published by the Free Software Foundation, either
-// version 3 of the License, or (at your option) any later version.
+//   version 3 of the License, or (at your option) any later version.
 // * Mozilla Public License as published by the Mozilla Foundation, version 2.
+// * The Patron License (https://github.com/notgull/piet-hardware/blob/main/LICENSE-PATRON.md)
+//   for sponsors and contributors, who can ignore the copyleft provisions of the above licenses
+//   for this project.
 //
-// `piet-hardware` is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// See the GNU Lesser General Public License or the Mozilla Public License for more details.
+// `piet-hardware` is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU Lesser General Public License or the Mozilla Public License for more
+// details.
 //
 // You should have received a copy of the GNU Lesser General Public License and the Mozilla
-// Public License along with `piet-hardware`. If not, see <https://www.gnu.org/licenses/> or
-// <https://www.mozilla.org/en-US/MPL/2.0/>.
+// Public License along with `piet-hardware`. If not, see <https://www.gnu.org/licenses/>.
 
 use piet::kurbo::{Point, Rect, Size};
 use piet::Error as Pierror;
