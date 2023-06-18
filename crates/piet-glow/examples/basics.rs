c51f0bd293416f289a0fdb44c4c7405a70560978
John Nunley <jtnunley01@gmail.com>
2023-06-17 19:41:44 -0700

    Add sample generation for glow

--- crates/piet-glow/examples/basics.rs
+++ crates/piet-glow/examples/basics.rs
@@ -17,7 +17,8 @@
 
 //! An example with a basic usage of the library.
 
-include!("util/setup_context.rs");
+#[path = "util/setup_context.rs"]
+mod util;
 
 use piet::kurbo::{Affine, BezPath, Point, Rect, Vec2};
 use piet::{
--- crates/piet-glow/examples/radial_gradient.rs
+++ crates/piet-glow/examples/radial_gradient.rs
@@ -17,7 +17,8 @@
 
 //! An example with a basic usage of the library.
 
-include!("util/setup_context.rs");
+#[path = "util/setup_context.rs"]
+mod util;
 
 use piet::kurbo::{Point, Rect, Vec2};
 use piet::{GradientStop, RenderContext as _};
